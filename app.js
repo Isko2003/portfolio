42080fff7eedd88ec4421fa838d5ce82df15acd1
Isko2003 <theazeisko2003@gmail.com>
2024-12-29 18:18:07 +0400

    duzelisler

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ function downloadCV() {
   link.download = "main-resume-ismayil.pdf";
   link.click();
 }
+
+AOS.init({
+  duration: 1000,
+  easing: "ease",
+  once: true,
+});
+
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 600) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
